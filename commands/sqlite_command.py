c390d23169b98706bb16dcec5a8b460e6126c471
Artem-new <84340009+Artem-new@users.noreply.github.com>
2023-10-29 13:35:54 +0300

    Получить информацию о предыдущем трафике

--- commands/sqlite_command.py
+++ commands/sqlite_command.py
@@ -3,33 +3,51 @@ import sqlite3
 
 traffic_infomation = []
 
+class Base:
 
-def creata_table():
-    connection = sqlite3.connect('Megafon.db')
-    coursor_connection = connection.cursor()
-    coursor_connection.execute("""
-                                  CREATE TABLE IF MOT EXISTS Traffic_information(
-                                  id integer PIMARY KEY,
-                                  number text NOT NULL,
-                                  traffic integer NOT NULL,
-                                  format text NOT NULL
-                                  );
-                                  CREATE TABLE IF MOT EXISTS Last_info(
-                                  id integer PIMARY KEY,
-                                  last_traffic integer NOT NULL,
-                                  format text NOT NULL
-                                  );
-                                  CREATE TABLE IF NOT EXISTS Traffic_limit(
-                                  id integer PIMARY KEY,
-                                  limit integer NOT NULL,
-                                  format text NOT NULL
+
+    def connection_base(self, command):
+        connection = sqlite3.connect('Megafon.db')
+        coursor_connection = connection.cursor()
+        coursor_connection.execute(command)
+
+
+    def creata_table(self):
+        connection = sqlite3.connect('Megafon.db')
+        coursor_connection = connection.cursor()
+        coursor_connection.execute("""
+                                      CREATE TABLE IF MOT EXISTS Traffic_information(
+                                          id integer PIMARY KEY,
+                                          number text NOT NULL,
+                                          traffic integer NOT NULL,
+                                          format text NOT NULL
+                                      );
+                                      CREATE TABLE IF MOT EXISTS Last_info(
+                                          id integer PIMARY KEY,
+                                          last_traffic integer NOT NULL,
+                                          format text NOT NULL
+                                      );
+                                      CREATE TABLE IF NOT EXISTS Traffic_limit(
+                                          id integer PIMARY KEY,
+                                          limit integer NOT NULL,
+                                          format text NOT NULL
                                """)
+        coursor_connection.close()
 
 
-def take_information():
-    connection = sqlite3.connect('Megafon')
-    coursor_connection = connection.cursor()
-    coursor_connection.execute("""
-                                  SELECT last_traffic FROM Last_info 
-                                """)
-    traffic_infomation.append(coursor_connection.fetchall())
+    def take_information(self):
+        connection = sqlite3.connect('Megafon')
+        coursor_connection = connection.cursor()
+        coursor_connection.execute("""
+                                      SELECT last_traffic FROM Last_info 
+                                    """)
+        traffic_infomation.append(coursor_connection.fetchall())
+        coursor_connection.close()
+
+
+    def save_information_in_the_table(self, numb, traffics, lust_traffic):
+        connection = sqlite3.connect('Megafon')
+        coursor_connection = connection.cursor()
+        coursor_connection.execute("""
+                                   
+                                   """)
