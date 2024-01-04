1e80b036835ebcef88087fb59de0056746bbf621
Artem-new <84340009+Artem-new@users.noreply.github.com>
2024-01-04 15:49:58 +0300

    Добаил команду сохранения данных в иаблицу о предыдущей информации

--- commands/sqlite_command.py
+++ commands/sqlite_command.py
@@ -1,8 +1,12 @@
 import sqlite3
+from datetime import date
 
 
 traffic_infomation = []
 information_about_limit_traffic = []
+
+time = date.today()
+
 
 def connection_base(command):
     connection = sqlite3.connect('Megafon.db')
@@ -15,6 +19,7 @@ def creata_table():
     coursor_connection = connection.cursor()
     coursor_connection.execute("""
                                   CREATE TABLE IF NOT EXISTS Traffic_information(
+                                      date text NOT NULL,
                                       number text NOT NULL,
                                       traffic text NOT NULL,
                                       format text NOT NULL
@@ -50,7 +55,7 @@ def save_information_in_the_table(numb, traffics):
     connection = sqlite3.connect('Megafon.db')
     coursor_connection = connection.cursor()
     '''Вставить в таблицу инофрмацию об использованом трафиком между предыдущими сессиями'''
-    coursor_connection.execute("INSERT INTO Traffic_information (number, traffic, format) VALUES (?,?,?)",(numb, traffics, 'Гб.'))
+    coursor_connection.execute("INSERT INTO Traffic_information (date, number, traffic, format) VALUES (?,?,?,?)", (time, numb, traffics, 'Гб.'))
     connection.commit()
 
 def save_information_in_the_table_about_limit(numb, limit, format):
@@ -72,12 +77,22 @@ def load_information_about_traffic_limit(numb):
         print(information_about_limit_traffic)
     connection.commit()
 
-def update_informatin_in_the_table_Lust_info(numb, lust_traffic):
+
+def save_information_about_last_info(numb, last_traffic):
     connection = sqlite3.connect('Megafon.db')
     coursor_connection = connection.cursor()
-    coursor_connection.execute(f'DELETE FROM Last_info WHERE number={numb}', ('newuser',))
-    coursor_connection.execute(f"UPDATE Last_info SET last_traffic=? WHERE number=?",
-                               (lust_traffic, numb))
+    coursor_connection.execute("INSERT INTO Last_info (number, last_traffic, format) VALUES (?,?,?)", (numb, last_traffic, 'Гб.'))
     connection.commit()
 
+
+def update_informatin_in_the_table_Lust_info(numb, lust_traffic):
+    try:
+        connection = sqlite3.connect('Megafon.db')
+        coursor_connection = connection.cursor()
+        coursor_connection.execute(f'DELETE FROM Last_info WHERE number={numb}', ('newuser',))
+        coursor_connection.execute(f"UPDATE Last_info SET last_traffic=? WHERE number=?",
+                              (lust_traffic, numb))
+        connection.commit()
+    except Exception:
+        save_information_about_last_info(numb, lust_traffic)
 
